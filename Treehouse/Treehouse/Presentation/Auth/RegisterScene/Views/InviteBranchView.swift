34f7298386653e0fc8f62d6c018ac1dbccbfd3e7
timotheekim10 <timotheekim10@gmail.com>
2024-04-19 01:29:18 +0900

    [Feat] #26 - 초대장 보내기 섹션 상단 UI 구현

--- Treehouse/Treehouse/Presentation/Auth/RegisterScene/Views/InviteBranchView.swift
+++ Treehouse/Treehouse/Presentation/Auth/RegisterScene/Views/InviteBranchView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct InviteBranchView: View {
     
     @State private var inviteCount: Int = 0
+    @State private var availableInviteCount: Int = 0
+    @State private var percent: CGFloat = 70
     
     var body: some View {
         ScrollView() {
@@ -59,6 +61,61 @@ struct InviteBranchView: View {
                 
                 Divider()
                     .foregroundColor(.gray3)
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack(spacing: 9) {
+                        Text("초대장 보내기")
+                            .fontWithLineHeight(fontLevel: .heading4)
+                            .foregroundStyle(.grayscaleBlack)
+                        
+                        Text("가진 초대장 : \(availableInviteCount)장")
+                            .fontWithLineHeight(fontLevel: .body2)
+                            .foregroundStyle(.treeGreen)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 21)
+                
+                Text(StringLiterals.Invitation.guidanceTitle1)
+                    .fontWithLineHeight(fontLevel: .body3)
+                    .foregroundStyle(.gray6)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 5)
+                
+                HStack(spacing: 27) {
+                    ZStack(alignment: .leading) {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(.gray2)
+                            .frame(width: SizeLiterals.Screen.screenWidth * 292/393)
+                            .frame(height: 10)
+                            .padding(.top, 16)
+                        
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(.treeBlack)
+                        
+                            .frame(width: percent/100 * SizeLiterals.Screen.screenWidth * 292/393)
+                            .frame(height: 10)
+                            .padding(.top, 16)
+                    }
+                    
+                    Image("ic_invitation")
+                        .offset(y: 7)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 42)
+                
+                HStack(spacing: 0) {
+                    Text("\(100 - Int(percent))%")
+                        .foregroundStyle(.treeGreen)
+                    +
+                    Text("만 더 채우면 초대장 한 장을 받아요.")
+                        .foregroundStyle(.treeBlack)
+                    
+                    Image("ic_tooltip")
+                }
+                .fontWithLineHeight(fontLevel: .body4)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
             }
             .padding(.horizontal, SizeLiterals.Screen.screenWidth * 16/33)
         }
