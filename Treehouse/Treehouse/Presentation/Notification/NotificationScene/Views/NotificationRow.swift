82f15e32a63325107e60e1bac41e06b435c984e1
0seoYun <yys020819@gmail.com>
2024-04-20 01:38:14 +0900

    [Feat] #27 - 알림 Row View 구현

---
+++ Treehouse/Treehouse/Presentation/Notification/NotificationScene/Views/NotificationRow.swift
@@ -0,0 +1,50 @@
+//
+//  NotificationRow.swift
+//  Treehouse
+//
+//  Created by 윤영서 on 4/18/24.
+//
+
+import SwiftUI
+
+struct NotificationRow: View {
+    
+    var notification: NotificationStruct
+    
+    // MARK: - View
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            notification.profileImage
+                .resizable()
+                .scaledToFill()
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+                .padding(.top, 3)
+                .padding(.trailing, 8)
+            
+            Text(notification.userName)
+                .font(.fontGuide(.body4))
+                .foregroundColor(.treeBlack)
+            
+            + Text(notification.type.notificationContent)
+                .font(.fontGuide(.body3))
+                .foregroundColor(.treeBlack)
+            
+            + Text(" \(notification.time)ㆍ")
+                .font(.fontGuide(.body3))
+                .foregroundColor(.gray6)
+            
+            + Text(notification.tree)
+                .font(.fontGuide(.body3))
+                .foregroundColor(.gray6)
+        }
+        .padding(EdgeInsets(top: 14, leading: 16, bottom: 16, trailing: 14))
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    NotificationRow(notification: NotificationStruct.notificationDummyData[4])
+}
