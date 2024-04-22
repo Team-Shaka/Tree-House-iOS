9820f01de3673aefb2b6d8bf51a7197b9190c17f
timotheekim10 <timotheekim10@gmail.com>
2024-04-23 00:51:10 +0900

    [Style] #26 - 편지 봉투 너비/높이 숫자 변수 생성

--- Treehouse/Treehouse/Presentation/Auth/InvitationScene/Views/GreenLetterView.swift
+++ Treehouse/Treehouse/Presentation/Auth/InvitationScene/Views/GreenLetterView.swift
@@ -9,21 +9,31 @@ import SwiftUI
 
 struct GreenLetterView: View {
     
+    // MARK: - Property
+    
+    let drawingWidth1 = SizeLiterals.Screen.screenWidth * 361/393
+    let drawingWidth2 = SizeLiterals.Screen.screenWidth * 15/393
+    let drawingWidth3 = SizeLiterals.Screen.screenWidth * 346/393
+    
+    let drawingHeight1 = SizeLiterals.Screen.screenHeight * 136/852
+    let drawingHeight2 = SizeLiterals.Screen.screenHeight * 121/852
+    let drawingHeight3 = SizeLiterals.Screen.screenHeight * 95/852
+    
     // MARK: - View
     
     var body: some View {
         Path { path in
             path.move(to: .zero)
-            path.addLine(to: CGPoint(x: 0, y: 121))
-            path.addQuadCurve(to: CGPoint(x: 15, y: 136), control: CGPoint(x: 0, y: 136))
-            path.addLine(to: CGPoint(x: 346, y: 136))
-            path.addQuadCurve(to: CGPoint(x: 361, y: 121), control: CGPoint(x: 361, y: 136))
-            path.addLine(to: CGPoint(x: 361, y: 0))
-            path.addLine(to: CGPoint(x: 361 / 2, y: 95))
+            path.addLine(to: CGPoint(x: 0, y: drawingHeight2))
+            path.addQuadCurve(to: CGPoint(x: drawingWidth2, y: drawingHeight1), control: CGPoint(x: 0, y: drawingHeight1))
+            path.addLine(to: CGPoint(x: drawingWidth3, y: drawingHeight1))
+            path.addQuadCurve(to: CGPoint(x: drawingWidth1, y: drawingHeight2), control: CGPoint(x: drawingWidth1, y: drawingHeight1))
+            path.addLine(to: CGPoint(x: drawingWidth1, y: 0))
+            path.addLine(to: CGPoint(x: drawingWidth1 / 2, y: drawingHeight3))
             path.closeSubpath()
         }
         .fill(.treeGreen)
-        .frame(width: 361, height: 136)
+        .frame(width: drawingWidth1, height: drawingHeight1)
         
     }
 
