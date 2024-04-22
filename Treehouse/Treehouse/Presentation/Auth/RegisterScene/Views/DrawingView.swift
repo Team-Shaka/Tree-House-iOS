ea2a8723fcf2752b7a2a851860295fe6f6d35231
Genesis <dmsgktn1685@naver.com>
2024-04-22 23:54:54 +0900

    [Fix] #28 - 자신의 View 크기로 부터 좌표 설정 로직으로 변경

--- Treehouse/Treehouse/Presentation/Auth/RegisterScene/Views/DrawingView.swift
+++ Treehouse/Treehouse/Presentation/Auth/RegisterScene/Views/DrawingView.swift
@@ -10,18 +10,18 @@ import SwiftUI
 struct DrawingView: View {
     var body: some View {
         Path { path in
-            let bothEndsHeight = SizeLiterals.Screen.screenHeight * (145)/852
-            let centerHeight = SizeLiterals.Screen.screenHeight * (56)/852
-            let safeAreaHeight = self.safeAreaHeight()
+            let startHeight = SizeLiterals.Screen.screenHeight * (139)/852
+            let centerHeight = SizeLiterals.Screen.screenHeight * (89)/852
             
-            path.move(to: CGPoint(x: 0, y: SizeLiterals.Screen.screenHeight - bothEndsHeight - safeAreaHeight))
-            path.addLine(to: CGPoint(x: 0, y: SizeLiterals.Screen.screenHeight))
-            path.addLine(to: CGPoint(x: SizeLiterals.Screen.screenWidth, y: SizeLiterals.Screen.screenHeight))
-            path.addLine(to: CGPoint(x: SizeLiterals.Screen.screenWidth, y: SizeLiterals.Screen.screenHeight - bothEndsHeight - safeAreaHeight))
-            path.addLine(to: CGPoint(x: SizeLiterals.Screen.screenWidth / 2, y: SizeLiterals.Screen.screenHeight - centerHeight - safeAreaHeight))
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: 0, y: startHeight))
+            path.addLine(to: CGPoint(x: SizeLiterals.Screen.screenWidth, y: startHeight))
+            path.addLine(to: CGPoint(x: SizeLiterals.Screen.screenWidth, y: 0))
+            path.addLine(to: CGPoint(x: SizeLiterals.Screen.screenWidth / 2, y: centerHeight))
             path.closeSubpath()
         }
         .fill(.treeGreen)
+        .frame(width: SizeLiterals.Screen.screenWidth, height: SizeLiterals.Screen.screenHeight * 139 / 852)
     }
 }
 
