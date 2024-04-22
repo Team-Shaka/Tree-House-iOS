c2b77bec3c5317f967f1f572a6b86c2b9b189d23
Genesis <dmsgktn1685@naver.com>
2024-04-22 23:55:55 +0900

    [Fix] #28 - View 구조 변경 및 InvitationView 로 View 분할

--- Treehouse/Treehouse/Presentation/Auth/RegisterScene/Views/ReceivedFirstInvitaionView.swift
+++ Treehouse/Treehouse/Presentation/Auth/RegisterScene/Views/ReceivedFirstInvitaionView.swift
@@ -14,81 +14,50 @@ struct ReceivedFirstInvitaionView: View {
     var iterater: Int {
         return memberNum >= 3 ? 3 : memberNum
     }
-    
-    // MARK: - State Property
-    
-    @State var treehouseName: String = "점심팟"
-    @State var invitedMember: String = "Chriiii0o0"
-    @State var memberNum: Int = 20
+
+    var treehouseName: String = "점심팟"
+    var invitedMember: String = "Chriiii0o0"
+    var memberNum: Int = 20
     
     // MARK: - View
     
     var body: some View {
         ZStack(alignment: .top) {
-            ZStack(alignment: .bottom) {
-                Rectangle()
-                    .fill(.treeDarkgreen)
-                    .frame(height: SizeLiterals.Screen.screenHeight * 145/852)
-                
-                ZStack(alignment: .top) {
-                    RoundedRectangle(cornerRadius: 12.0)
-                        .stroke(.gray3, lineWidth: 1)
-                        .fill(.grayscaleWhite)
-                    
-                    VStack(spacing: 0) {
-                        treehouseTitleView
-                            .padding(.top, SizeLiterals.Screen.screenHeight * 26.44 / 852)
-                            .padding(.horizontal, 21)
-                        
-                        invitationView
-                            .padding(.bottom, SizeLiterals.Screen.screenHeight * (46)/852)
-                        
-                        HStack(spacing: 12) {
-                            Button(action: {
-                                
-                            }) {
-                                Text(StringLiterals.Register.buttonTitle7)
-                                    .fontWithLineHeight(fontLevel: .body3)
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 48)
-                                    .foregroundStyle(.treeBlack)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 8)
-                                            .stroke(.treeBlack, lineWidth: 1.5)
-                                    )
-                                    .cornerRadius(8)
-                            }
-                            
-                            Button(action: {
-                                
-                            }) {
-                                Text(StringLiterals.Register.buttonTitle8)
-                                    .fontWithLineHeight(fontLevel: .body3)
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 48)
-                                    .foregroundStyle(.white)
-                                    .background(.treeBlack)
-                                    .cornerRadius(8)
-                            }
-                        }
-                        .padding(.horizontal, 14)
-                    }
-                }
-                .background(.gray1)
-                .padding(.horizontal, 20)
-                .padding(.top, SizeLiterals.Screen.screenHeight * 234 / 852)
-                
-                DrawingView()
-            }
-            
             topHeaderView
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.leading, 24)
-                .padding(.top, SizeLiterals.Screen.screenHeight * 66 / 852)
+                .padding(.top, SizeLiterals.Screen.screenHeight * 23 / 852)
                 .padding(.bottom, SizeLiterals.Screen.screenHeight * 20 / 852)
+            
+            ZStack(alignment: .bottom) {
+                Color.clear
+                
+                Rectangle()
+                    .fill(.treeDarkgreen)
+                    .frame(height: SizeLiterals.Screen.screenHeight * 139 / 852)
+                
+                InvitationView(invitationType: .first,
+                               treehouseName: treehouseName,
+                               invitedMember: invitedMember,
+                               memberNum: memberNum)
+                
+                DrawingView()
+            }
         }
         .ignoresSafeArea(.all, edges: .bottom)
         .background(.gray1)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.black)
+                        .frame(width: 24, height: 24)
+                }
+                .padding(.top, 11)
+            }
+        }
     }
 }
 
@@ -126,6 +95,7 @@ private extension ReceivedFirstInvitaionView {
             HStack(spacing: 0) {
                 Text("\(invitedMember)님")
                     .fontWithLineHeight(fontLevel: .body2)
+                
                 Text("이 당신을 초대했습니다.")
                     .fontWithLineHeight(fontLevel: .body3)
             }
@@ -189,5 +159,7 @@ private extension ReceivedFirstInvitaionView {
 // MARK: - Preview
 
 #Preview {
-    ReceivedFirstInvitaionView()
+    NavigationStack {
+        ReceivedFirstInvitaionView()
+    }
 }
