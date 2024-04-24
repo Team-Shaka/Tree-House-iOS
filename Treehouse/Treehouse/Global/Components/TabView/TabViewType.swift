ca3dda8623a61c693fa5fed07e968297df8159b0
0seoYun <yys020819@gmail.com>
2024-04-25 01:42:35 +0900

    [Feat] #31 - TreeTabView 구현

---
+++ Treehouse/Treehouse/Global/Components/TabView/TabViewType.swift
@@ -0,0 +1,33 @@
+//
+//  TabViewStruct.swift
+//  Treehouse
+//
+//  Created by 윤영서 on 4/23/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeTab: View {
+    var body: some View {
+        Text("HomeView")
+    }
+}
+
+struct TreeTab: View {
+    var body: some View {
+        Text("TreeView")
+    }
+}
+
+struct NotificationTab: View {
+    var body: some View {
+        NotificationView()
+    }
+}
+
+struct SettingsTab: View {
+    var body: some View {
+        Text("SettingsView")
+    }
+}
