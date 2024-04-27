4f25532a851851f9b59349e9049effd3de2542d2
timotheekim10 <timotheekim10@gmail.com>
2024-04-27 23:15:59 +0900

    [Feat] #36 - CheckNameRequestEntity 구현

---
+++ Treehouse/Treehouse/Domain/Entities/Register/CheckNameRequestEntity.swift
@@ -0,0 +1,12 @@
+//
+//  CheckNameRequestEntity.swift
+//  Treehouse
+//
+//  Created by 티모시 킴 on 4/27/24.
+//
+
+import Foundation
+
+struct CheckNameRequestEntity: Codable {
+    let userName: String
+}
