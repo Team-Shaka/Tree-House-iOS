81a01f0370552cb0a98e8f27c8aad025e45177ec
Genesis <dmsgktn1685@naver.com>
2024-05-22 21:17:42 +0900

    [Feat] #47 - Feed Response DTO 구현

---
+++ Treehouse/Treehouse/Network/Feed/DTO/Response/PatchUpdateFeedPostResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PatchUpdateFeedPostResponseDTO.swift
+//  Treehouse
+//
+//  Created by ParkJunHyuk on 5/22/24.
+//
+
+import Foundation
+
+struct PatchUpdateFeedPostResponseDTO: Decodable {
+    let context: String
+}
---
+++ Treehouse/Treehouse/Network/Feed/DTO/Response/PostCreateFeedPostsResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PostCreateFeedPostsResponseDTO.swift
+//  Treehouse
+//
+//  Created by ParkJunHyuk on 5/22/24.
+//
+
+import Foundation
+
+struct PostCreateFeedPostsResponseDTO: Decodable {
+    let postId: Int
+}
