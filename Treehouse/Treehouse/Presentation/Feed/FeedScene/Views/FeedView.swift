d749e2f360b9d2c0c8ec24be26651caad66f9eb0
0seoYun <yys020819@gmail.com>
2024-06-03 01:49:25 +0900

    [Feat] #49 - keyboard tool bar 구현

--- Treehouse/Treehouse/Presentation/Feed/FeedScene/Views/FeedView.swift
+++ Treehouse/Treehouse/Presentation/Feed/FeedScene/Views/FeedView.swift
@@ -48,18 +48,40 @@ struct FeedView: View {
 extension FeedView {
     @ViewBuilder
     private var postTextField: some View {
-        HStack(spacing: 0) {
-            Image(.imgDummy2)
-                .resizable()
-                .clipShape(Circle())
-                .frame(width: 36, height: 36)
-            
-            TextField("groupname에 글쓰기...", text: $postContent)
-                .font(.fontGuide(.body5))
-                .padding()
-                .tint(.treeGreen)
-                .foregroundColor(textFieldState.fontColor)
-                .focused($focusedField, equals: .post)
+        VStack(spacing: 0) {
+            HStack(spacing: 12) {
+                Image(.imgDummy2)
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 36, height: 36)
+                
+                TextField("groupname에 글쓰기...", text: $postContent)
+                    .font(.fontGuide(.body5))
+                    .tint(.treeGreen)
+                    .foregroundColor(textFieldState.fontColor)
+                    .focused($focusedField, equals: .post)
+                    .keyboardType(.asciiCapable)
+                    .autocorrectionDisabled(true)
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer()
+                            Button(action: {
+                            }){
+                                Image(.icPhoto)
+                            }
+                            Button(action: {
+                            }){
+                                Image(.icDelete2)
+                            }
+                        }
+                    }
+                                
+                if textFieldState == .enable {
+                    Image(.icUpload)
+                        .padding(.trailing, 16)
+                }
+            }
+            .padding(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
             
             if textFieldState == .enable {
                 Image(.icUpload)
