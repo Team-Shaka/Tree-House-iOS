5a2ad2605d5daeca8a75f765ab27d23c70c6eeaf
timotheekim10 <timotheekim10@gmail.com>
2024-06-14 17:02:43 +0900

    [Fix] #54 - MyProfileView 바이오 뷰 수정

--- Treehouse/Treehouse/Presentation/Profile/Views/MyProfileView.swift
+++ Treehouse/Treehouse/Presentation/Profile/Views/MyProfileView.swift
@@ -45,8 +45,6 @@ struct MyProfileView: View {
                                     .fontWithLineHeight(fontLevel: .body5)
                                     .foregroundColor(.grayscaleBlack)
                                     .padding(12)
-                                    .lineLimit(nil)
-                                    .fixedSize(horizontal: false, vertical: true)
                                     .frame(width: SizeLiterals.Screen.screenWidth * 268 / 393, alignment: .leading)
                             }
                         }
