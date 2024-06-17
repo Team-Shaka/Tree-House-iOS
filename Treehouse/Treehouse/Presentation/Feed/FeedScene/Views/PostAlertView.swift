1b0347a821b1e45fb6c51aeee7fcb321e75175ee
0seoYun <yys020819@gmail.com>
2024-06-17 14:14:30 +0900

    [Feat] #58 - 피드뷰 내 공용 알러트뷰 구현

---
+++ Treehouse/Treehouse/Presentation/Feed/FeedScene/Views/PostAlertView.swift
@@ -0,0 +1,74 @@
+//
+//  PostAlertView.swift
+//  Treehouse
+//
+//  Created by 윤영서 on 6/13/24.
+//
+
+import SwiftUI
+import PopupView
+
+struct PostAlertView: View {
+        
+    // MARK: - Property
+       
+       var alertContent: String
+       var onCancel: () -> Void
+       var onConfirm: () -> Void
+    
+    // MARK: - View
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12.0)
+                .frame(width: 351, height: 178)
+                .foregroundStyle(.gray1)
+            // TODO: - ForegroundStyle white로 변경
+            
+            VStack(spacing: 41) {
+                Text(alertContent)
+                    .font(.fontGuide(.heading4))
+                
+                HStack(spacing: 12) {
+                    Button(action: {
+                        onCancel()
+                    }) {
+                        Text("취소")
+                            .font(.fontGuide(.body3))
+                            .foregroundStyle(.treeBlack)
+                            .padding(EdgeInsets(top: 11.0, leading: 64.0, bottom: 11.0, trailing: 64.0))
+                            .background(.grayscaleWhite)
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(.treeBlack, lineWidth: 1.5)
+                    )
+                    .cornerRadius(8)
+                    
+                    Button(action: {
+                        onConfirm()
+                    }) {
+                        Text("확인")
+                            .font(.fontGuide(.body3))
+                            .foregroundStyle(.grayscaleWhite)
+                            .padding(EdgeInsets(top: 11.0, leading: 64.0, bottom: 11.0, trailing: 64.0))
+                            .background(.grayscaleBlack)
+                            .border(.black)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                }
+            }
+            .padding(EdgeInsets(top: 49.0, leading: 14.0, bottom: 14.0, trailing: 14.0))
+        }
+    }
+}
+
+
+// MARK: - Preview
+
+#Preview {
+    PostAlertView(
+        alertContent: "수정한 내용을 삭제하시겠어요?",
+        onCancel: {},
+        onConfirm: {}
+    )}
