6340476e74e093eea7951160664146a19386b2e9
Genesis <dmsgktn1685@naver.com>
2024-06-18 22:10:37 +0900

    [Fix] #60 -  API 통신 순서를 위한 print 구문 추가 및 FeedAPI URL 수정

--- Treehouse/Treehouse/Network/Register/RegisterService.swift
+++ Treehouse/Treehouse/Network/Register/RegisterService.swift
@@ -12,6 +12,10 @@ class RegisterService: NetworkServiceable {
     
     /// 유저 아이디 중복 확인 요청 API
     func postCheckName(userName: String) async throws -> PostCheckUserNameResponseDTO {
+        
+        print("1️⃣ PostCheckName API 호출 ========================================")
+        print("Input - userName: \(userName)")
+        
         // 요청 DTO 생성
         let requestDTO = PostCheckUserNameRequestDTO(userName: userName)
         
@@ -28,16 +32,18 @@ class RegisterService: NetworkServiceable {
         
         // 응답 데이터와 상태 코드 출력
         if let httpResponse = response as? HTTPURLResponse {
-            print("Status Code: \(httpResponse.statusCode)")
+            print("2️⃣ Status Code: \(httpResponse.statusCode)")
         }
         
         if let jsonString = String(data: data, encoding: .utf8) {
-            print("Response JSON: \(jsonString)")
+            print("3️⃣ Response JSON")
         }
         
         // JSON 디코딩
         do {
             let model = try JSONDecoder().decode(BaseResponse<PostCheckUserNameResponseDTO>.self, from: data)
+            print(model.data)
+            print("4️⃣ PostCheckName API 종료 ========================================")
             return model.data
         } catch {
             throw NetworkError.jsonDecodingError
@@ -46,6 +52,9 @@ class RegisterService: NetworkServiceable {
     
     /// 토큰 재발급 요청 API
     func postReissueToken() async throws -> PostReissueTokenResponseDTO {
+        
+        print("1️⃣ PostReissueToken API 호출 ========================================")
+        
         // RefreshToken 이 저장되어있는지 확인
         guard let loadRefreshToken = KeychainHelper.shared.load(for: Config.refreshTokenKey) else {
             throw NetworkError.clientError(message: "토큰 없음")
@@ -61,24 +70,32 @@ class RegisterService: NetworkServiceable {
         
         // 응답 데이터와 상태 코드 출력
         if let httpResponse = response as? HTTPURLResponse {
-            print("Status Code: \(httpResponse.statusCode)")
+            print("2️⃣ Status Code: \(httpResponse.statusCode)")
+            print("\(httpResponse.statusCode)")
         }
         
         if let jsonString = String(data: data, encoding: .utf8) {
-            print("Response JSON: \(jsonString)")
+            print("3️⃣ Response JSON")
         }
         
         // JSON 디코딩
         do {
             let model = try JSONDecoder().decode(BaseResponse<PostReissueTokenResponseDTO>.self, from: data)
+            print(model.data)
+            print("4️⃣ PostReissueToken API 종료 ========================================")
             return model.data
         } catch {
+            print("4️⃣ PostReissueToken API Error: \(String(describing: NetworkError.jsonDecodingError.errorDescription))========================================")
             throw NetworkError.jsonDecodingError
         }
     }
     
     /// 신규 유저 회원가입 요청 API
     func postRegisterUser(phoneNumber: String, userName: String) async throws -> PostRegisterUserResponseDTO {
+        
+        print("1️⃣ PostRegisterUser API 호출 ========================================")
+        print("Input - phoneNumber: \(phoneNumber), userName: \(userName)")
+        
         let request = NetworkRequest(requestType: RegisterAPI.postRegisterUser(requestBody: PostRegisterUserRequestDTO(phoneNumber: phoneNumber, userName: userName)))
         
         guard let urlRequest = request.request() else {
@@ -89,24 +106,31 @@ class RegisterService: NetworkServiceable {
         
         // 응답 데이터와 상태 코드 출력
         if let httpResponse = response as? HTTPURLResponse {
-            print("Status Code: \(httpResponse.statusCode)")
+            print("2️⃣ Status Code: \(httpResponse.statusCode)")
+            print("\(httpResponse.statusCode)")
         }
         
         if let jsonString = String(data: data, encoding: .utf8) {
-            print("Response JSON: \(jsonString)")
+            print("3️⃣ Response JSON")
         }
         
         // JSON 디코딩
         do {
             let model = try JSONDecoder().decode(BaseResponse<PostRegisterUserResponseDTO>.self, from: data)
+            print(model.data)
+            print("4️⃣ PostRegisterUser API 종료 ========================================")
             return model.data
         } catch {
+            print("4️⃣ PostRegisterUser API Error: \(String(describing: NetworkError.jsonDecodingError.errorDescription))========================================")
             throw NetworkError.jsonDecodingError
         }
     }
 
     /// 트리하우스 멤버 가입 API
     func postRegisterTreeMember(requestBody: PostRegisterTreeMemberRequestDTO) async throws -> PostRegisterTreeMemberResponseDTO {
+        
+        print("1️⃣ PostRegisterTreeMember API 호출 ========================================")
+        print("Input - PostRegisterTreeMemberRequestDTO: \(requestBody)")
         let request = NetworkRequest(requestType: RegisterAPI.postRegisterTreeMember(requestBody: requestBody))
         
         guard let urlRequest = request.request() else {
@@ -117,24 +141,30 @@ class RegisterService: NetworkServiceable {
         
         // 응답 데이터와 상태 코드 출력
         if let httpResponse = response as? HTTPURLResponse {
-            print("Status Code: \(httpResponse.statusCode)")
+            print("2️⃣ Status Code: \(httpResponse.statusCode)")
         }
         
         if let jsonString = String(data: data, encoding: .utf8) {
-            print("Response JSON: \(jsonString)")
+            print("3️⃣ Response JSON")
+            print(jsonString)
         }
         
         // JSON 디코딩
         do {
             let model = try JSONDecoder().decode(BaseResponse<PostRegisterTreeMemberResponseDTO>.self, from: data)
+            print(model.data)
+            print("4️⃣ PostRegisterTreeMember API 종료 ========================================")
             return model.data
         } catch {
+            print("4️⃣ PostRegisterTreeMember API Error: \(String(describing: NetworkError.jsonDecodingError.errorDescription))========================================")
             throw NetworkError.jsonDecodingError
         }
     }
     
     /// 이전에 회원가입이 되어있는지 전화번호 인증 API
     func postCheckUserPhone(phoneNumber: String) async throws -> PostCheckUserPhoneResponseDTO {
+        print("1️⃣ PostRegisterTreeMember API 호출 ========================================")
+        
         let request = NetworkRequest(requestType: RegisterAPI.postCheckUserPhone(requestBody: PostCheckUserPhoneRequestDTO(phoneNumber: phoneNumber)))
         
         guard let urlRequest = request.request() else {
@@ -145,18 +175,21 @@ class RegisterService: NetworkServiceable {
         
         // 응답 데이터와 상태 코드 출력
         if let httpResponse = response as? HTTPURLResponse {
-            print("Status Code: \(httpResponse.statusCode)")
+            print("2️⃣ Status Code: \(httpResponse.statusCode)")
         }
         
         if let jsonString = String(data: data, encoding: .utf8) {
-            print("Response JSON: \(jsonString)")
+            print("3️⃣ Response JSON")
         }
         
         // JSON 디코딩
         do {
             let model = try JSONDecoder().decode(BaseResponse<PostCheckUserPhoneResponseDTO>.self, from: data)
+            print(model.data)
+            print("4️⃣ PostRegisterTreeMember API 종료 ========================================")
             return model.data
         } catch {
+            print("4️⃣ PostRegisterTreeMember API Error: \(String(describing: NetworkError.jsonDecodingError.errorDescription))========================================")
             throw NetworkError.jsonDecodingError
         }
     }
