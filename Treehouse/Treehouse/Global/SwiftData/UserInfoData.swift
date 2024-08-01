52e491ae3dc78adde2744e8f1ff4b6407c6e94ab
Genesis <dmsgktn1685@naver.com>
2024-08-01 17:30:03 +0900

    [Fix] #84 - UserInfo SwiftData 모델 수정

--- Treehouse/Treehouse/Global/SwiftData/UserInfoData.swift
+++ Treehouse/Treehouse/Global/SwiftData/UserInfoData.swift
@@ -10,25 +10,41 @@ import SwiftUI
 
 @Model
 class UserInfoData {
-    var userId: Int
+    @Attribute(.unique) let userId: Int
     var userName: String
-    var treeMemberName: String
-    var treehouseId: [Treehouse]
-    var bio: String
-    @Attribute(.externalStorage)
-    var profileImageData: Data
+    var profileImageUrl: String
+    var treehouses: [Int] = []
+    @Relationship(deleteRule: .cascade) var treehouseInfo: [TreehouseInfo] = []
     
-    init(userId: Int, userName: String, treeMemberName: String, treehouseId: [Treehouse], bio: String, profileImageData: Data) {
+    init(userId: Int, userName: String, profileImageUrl: String, treehouses: [Int], treehouseInfo: [TreehouseInfo]) {
         self.userId = userId
         self.userName = userName
-        self.treeMemberName = treeMemberName
-        self.treehouseId = treehouseId
-        self.bio = bio
-        self.profileImageData = profileImageData
+        self.profileImageUrl = profileImageUrl
+        self.treehouses = treehouses
+        self.treehouseInfo = treehouseInfo
     }
 }
 
-struct Treehouse: Codable {
-    let treehouseId: Int
-    let treehouseName: String
+@Model
+class TreehouseInfo {
+    var treehouseId: Int
+    var treehouseMemberId: Int
+    var treehouseName: String
+    var bio: String
+    var profileImageUrl: URL?
+    @Relationship(inverse: \UserInfoData.treehouseInfo) var user: UserInfoData?
+    
+    init(treehouseId: Int, treehouseMemberId: Int, treehouseName: String, bio: String, profileImageUrl: URL?) {
+        self.treehouseId = treehouseId
+        self.treehouseMemberId = treehouseMemberId
+        self.treehouseName = treehouseName
+        self.bio = bio
+        self.profileImageUrl = profileImageUrl
+    }
+}
+
+extension UserInfoData {
+    func findTreehouse(id: Int) -> TreehouseInfo? {
+        return treehouseInfo.first { $0.treehouseId == id }
+    }
 }
