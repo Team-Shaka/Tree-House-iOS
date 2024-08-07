740ba854e97833570d7839d10532d550c3be3dea
Genesis <dmsgktn1685@naver.com>
2024-08-07 17:31:53 +0900

    [Feat] #101 - 앱 진입점에 Firebase 코드 추가

--- Treehouse/Treehouse/Application/TreehouseApp.swift
+++ Treehouse/Treehouse/Application/TreehouseApp.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct TreehouseApp: App {
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State var viewRouter = ViewRouter()
     @AppStorage(Config.loginKey) private var isLogin = false
     
