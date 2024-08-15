131900d59120c0ff7d85a36c6f4b931bc8478af5
Genesis <dmsgktn1685@naver.com>
2024-08-15 18:12:09 +0900

    [Feat] #92 - ToolBar 에서 뒤로가기 제스처 추가

---
+++ Treehouse/Treehouse/Global/Extension/UINavigationController+.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+.swift
+//  Treehouse
+//
+//  Created by ParkJunHyuk on 8/15/24.
+//
+
+import SwiftUI
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
