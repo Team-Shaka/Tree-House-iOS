7824a6018c5f56dfaba3d25239a495ef796edd16
Genesis <dmsgktn1685@naver.com>
2024-09-09 16:29:46 +0900

    [Fix] ci_post_clone.sh 파일 수정

--- Treehouse/ci_scripts/ci_post_clone.sh
+++ Treehouse/ci_scripts/ci_post_clone.sh
@@ -4,27 +4,17 @@
 #  Treehouse
 #
 #  Created by ParkJunHyuk on 9/9/24.
-#  
+#
 
-# *.xconfig 파일이 생성될 폴더 경로
-FOLDER_PATH= "/Volumes/workspace/repository/Treehouse"
+# Config.xcconfig 파일 생성
+echo "환경변수 참조 Config.xcconfig file 생성시작"
+# Secrets 경로 지정
+cat <<EOF > "/Volumes/workspace/repository/Treehouse/Treehouse/Global/Resources/Config.xcconfig"
 
-# PARTS 배열의 두 번째 요소가 "Scheme Name"에 해당
-IFS='-' read -ra PARTS <<< "$CI_XCODE_SCHEME"
+BASE_URL = $(BASE_URL)
+ACCESS_TOKEN_KEY = $(ACCESS_TOKEN_KEY)
+REFRESH_TOKEN_KEY = $(REFRESH_TOKEN_KEY)
+LOGIN_KEY = $(LOGIN_KEY)
+WEB_FRONT_URL = $(WEB_FRONT_URL)
 
-# *.xconfig 파일 이름
-CONFIG_FILENAME="${PARTS[1]}.xcconfig"
-
-# *.xconfig 파일의 전체 경로 계산
-CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
-
-# 환경 변수에서 값을 가져와서 *.xconfig 파일에 추가하기
-echo "BASE_URL = $BASE_URL" >> "$CONFIG_FILE_PATH"
-echo "ACCESS_TOKEN_KEY = $ACCESS_TOKEN_KEY" >> "$CONFIG_FILE_PATH"
-echo "REFRESH_TOKEN_KEY = $REFRESH_TOKEN_KEY" >> "$CONFIG_FILE_PATH"
-echo "LOGIN_KEY = $LOGIN_KEY" >> "$CONFIG_FILE_PATH"
-echo "WEB_FRONT_URL = $WEB_FRONT_URL" >> "$CONFIG_FILE_PATH"
-
-# 생성된 *.xconfig 파일 내용 출력
-cat "$CONFIG_FILE_PATH"
-echo "${PARTS[1]}.xcconfig 파일이 성공적으로 생성되었고, 환경변수 값이 확인되었습니다."
+echo "환경변수 참조 Secrets.xcconfig file 생성완료"
