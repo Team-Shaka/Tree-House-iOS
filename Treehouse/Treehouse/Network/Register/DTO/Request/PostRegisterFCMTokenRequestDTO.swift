4668c15169b583ca50be4419ff06b6ca88b1d703
Genesis <dmsgktn1685@naver.com>
2024-09-10 09:04:26 +0900

    [Feat] #121 - FCM Token 저장 & Push 알람  동의 API 구현

---
+++ Treehouse/Treehouse/Network/Register/DTO/Request/PostRegisterFCMTokenRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PostRegisterFCMTokenRequestDTO.swift
+//  Treehouse
+//
+//  Created by ParkJunHyuk on 9/10/24.
+//
+
+import Foundation
+
+struct PostRegisterFCMTokenRequestDTO: Codable {
+    let fcmToken: String
+}
---
+++ Treehouse/Treehouse/Network/Register/DTO/Request/PostRegisterPushAgreeRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PostRegisterPushAgreeRequestDTO.swift
+//  Treehouse
+//
+//  Created by ParkJunHyuk on 9/10/24.
+//
+
+import Foundation
+
+struct PostRegisterPushAgreeRequestDTO: Codable {
+    let pushAgree: Bool
+}
